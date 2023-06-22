d75af096bb667cdd18381f42f27fd864299e4179
Elikem Savie <elikem@menaget.com>
2023-06-22 11:25:17 +0000

    Rename at Package Level

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MangoPayVaultSDK",
+    name: "MangopayVaultSDK",
     defaultLocalization: "en",
     platforms: [
         .macOS(.v10_15),
